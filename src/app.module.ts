88590d5e71532f57d75eb3103a9770ffb0a8073d
VasylArtymovych <art777vasss@gmail.com>
2022-12-16 21:15:30 +0200

    add staticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,14 @@ import { TracksModule } from './tracks/tracks.module';
 import config from './config/configuration';
 import { FileModule } from './file/file.module';
 import { AlbumModule } from './album/album.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     MongooseModule.forRoot(config().database.host),
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, 'static') }),
     TracksModule,
     FileModule,
     AlbumModule,
