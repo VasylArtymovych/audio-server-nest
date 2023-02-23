8577151ae119aa6428a5ed469bf77dca098b5422
VasylArtymovych <art777vasss@gmail.com>
2023-02-23 13:36:17 +0200

    update deploy config

--- src/tracks/tracks.module.ts
+++ src/tracks/tracks.module.ts
@@ -1,12 +1,12 @@
 import {
-  MiddlewareConsumer,
+  // MiddlewareConsumer,
   Module,
-  NestModule,
-  RequestMethod,
+  // NestModule,
+  // RequestMethod,
 } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FileService } from 'src/file/file.service';
-import { isValidId } from 'src/middleware/isValidId.middleware';
+// import { isValidId } from 'src/middleware/isValidId.middleware';
 import { Comment, CommentSchema } from './schema/comment.schema';
 import { Track, TrackSchema } from './schema/track.schema';
 import { TracksController } from './tracks.controller';
@@ -23,14 +23,15 @@ import { TracksService } from './tracks.service';
   providers: [TracksService, FileService],
   exports: [MongooseModule],
 })
-export class TracksModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(isValidId)
-      .forRoutes(
-        { path: 'tracks/:id', method: RequestMethod.PATCH },
-        { path: 'tracks/:id', method: RequestMethod.DELETE },
-        { path: 'tracks/listen/:id', method: RequestMethod.POST },
-      );
-  }
-}
+export class TracksModule {}
+// export class TracksModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer
+//       .apply(isValidId)
+//       .forRoutes(
+//         { path: 'tracks/:id', method: RequestMethod.PATCH },
+//         { path: 'tracks/:id', method: RequestMethod.DELETE },
+//         { path: 'tracks/listen/:id', method: RequestMethod.POST },
+//       );
+//   }
+// }
